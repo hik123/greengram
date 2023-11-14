bdbf063a36b6dcace71174e8a4d50f80c6af9799
firstt <khi05040@naver.com>
2023-11-14 17:52:50 +0900

    11-14 greengram

---
+++ src/main/java/com/green/greengram/feed/model/FeedPicsVo.java
@@ -0,0 +1,16 @@
+package com.green.greengram.feed.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class FeedPicsVo {
+    private int ifeed;
+    private String pic;
+}
